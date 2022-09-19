9f4fa8abf5e14480bf04527c68a4688caa9ba763
SONDONGHYUN <sun9999042@gmail.com>
2022-09-19 23:33:35 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ let count =0;
+  return (
+    <div className="App">
+     {count}
+  
+    </div>
+  );
+}
+
+export default App;
